120403da16c2a9147f6117cd8477644bc8c3f9df
nariomasahisa <nariomasahisa@gmail.com>
2023-01-22 17:24:21 +0900

    feat: add link header

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return (
+        <header className="h-10 bg-lime-200">
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </header>
+    );
+};
