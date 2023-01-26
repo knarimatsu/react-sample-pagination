b8ea9f35d4111421751edd981a4d8dee37c5e5ea
nariomasahisa <nariomasahisa@gmail.com>
2023-01-26 13:27:44 +0900

    chore: move App.tsx into pages directory

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,9 @@
-import { Header } from "./components/header";
+import { Header } from "../components/header";
 
 function App() {
     return (
         <>
-            <Header />
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <h2>Some</h2>
         </>
     );
 }
