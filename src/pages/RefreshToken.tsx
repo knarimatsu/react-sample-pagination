26e974595e7c9a28e1c1a84bb4371f48e81d7257
nariomasahisa <nariomasahisa@gmail.com>
2023-01-26 13:28:22 +0900

    chore: create Refresh Token page

---
+++ src/pages/RefreshToken.tsx
@@ -0,0 +1,9 @@
+export const RefreshToken = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Refresh Token</h1>
+        </>
+    );
+};
+
+export default RefreshToken;
