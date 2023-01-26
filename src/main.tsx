a08a50c65c9a8293551ed117087bee2ceeed5857
nariomasahisa <nariomasahisa@gmail.com>
2023-01-26 13:27:18 +0900

    feat: set router and page paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./pages/App";
 import "./index.css";
 import "./i18n/config";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RefreshToken from "./pages/RefreshToken";
+import StockList from "./pages/StockList";
+import { Header } from "./components/header";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/refresh-token",
+        element: <RefreshToken />,
+    },
+    {
+        path: "/stocks",
+        element: <StockList />,
+    },
+]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <Header />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
