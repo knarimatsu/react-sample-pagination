54037ddbf0c96179449d379c165ff66d1da1711c
nariomasahisa <nariomasahisa@gmail.com>
2023-02-05 15:56:36 +0900

    fix: remove codes

--- src/main.tsx
+++ src/main.tsx
@@ -3,32 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 import "./index.css";
 import "./i18n/config";
-import {
-    BrowserRouter,
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import RefreshToken from "./pages/RefreshToken";
-import StockList from "./pages/StockList";
-import { Header } from "./components/header";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-    {
-        path: "/refresh-token",
-        element: <RefreshToken />,
-    },
-    {
-        path: "/stocks",
-        element: <StockList />,
-    },
-]);
+import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
     </React.StrictMode>
 );
