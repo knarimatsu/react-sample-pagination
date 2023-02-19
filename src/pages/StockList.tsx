040e382b9b94fa768f18c098a4bd1a8a2fe654d0
nariomasahisa <nariomasahisa@gmail.com>
2023-02-20 00:12:58 +0900

    fix add form

--- src/pages/StockList.tsx
+++ src/pages/StockList.tsx
@@ -1,7 +1,22 @@
+import { useForm } from "react-hook-form";
+
 const StockList = () => {
+    const { register, handleSubmit, watch } = useForm();
+    const onSubmit = (data: any) => console.log(data);
     return (
         <>
             <h1 className="text-3xl font-bold underline">Stock List</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                    type="text"
+                    {...register("refreshToken")}
+                />
+                <input
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    type="submit"
+                />
+            </form>
         </>
     );
 };
