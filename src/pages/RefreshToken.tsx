3f3ecde4bfadc2829649e9fc443b0cd812c6a7f3
nariomasahisa <nariomasahisa@gmail.com>
2023-02-20 12:34:56 +0900

    fix add idToken form and copy button

--- src/pages/RefreshToken.tsx
+++ src/pages/RefreshToken.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { useRecoilState } from "recoil";
-import { refreshTokenState } from "../libs/recoilAtoms";
+import { idTokenState, refreshTokenState } from "../libs/recoilAtoms";
 import { getIdTokenAPI, getRefreshTokenAPI } from "../modules/api";
 
 export const RefreshToken = () => {
@@ -10,6 +10,8 @@ export const RefreshToken = () => {
     const { register, handleSubmit, watch } = useForm();
     const onSubmit = (data: any) => getIdToken(data.refreshToken);
     const [refreshToken, setRefreshToken] = useRecoilState(refreshTokenState);
+    const [idToken, setIdToken] = useRecoilState(idTokenState);
+
     /**
      * getRefreshToken
      * @returns {Promise} refreshToken
@@ -18,6 +20,7 @@ export const RefreshToken = () => {
         const result = await getRefreshTokenAPI();
         setRefreshToken(result.refreshToken);
     };
+
     /**
      * getIdToken
      * @param {string} refreshToken
@@ -25,10 +28,15 @@ export const RefreshToken = () => {
      */
     const getIdToken = async (refreshToken: string) => {
         const result = await getIdTokenAPI(refreshToken);
+        setIdToken(result.idToken);
     };
 
     const copyRefreshToken = () => {
         navigator.clipboard.writeText(refreshToken);
+    };
+
+    const copyIdToken = () => {
+        navigator.clipboard.writeText(idToken);
     };
 
     return (
@@ -85,6 +93,34 @@ export const RefreshToken = () => {
                     value={t("pages.refreshToken.getIdTokenButton") as string}
                 />
             </form>
+            {idToken ? (
+                <div className="flex border-solid">
+                    <input
+                        className="mx-3 mt-3 px-3 py-2 w-1/2 border-solid"
+                        type="text"
+                        readOnly
+                        value={idToken}
+                    />
+
+                    <div className="mt-3 p-3 border-solid hover:bg-slate-200">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-7 h-7 cursor-pointer"
+                            onClick={copyIdToken}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            ) : null}
         </>
     );
 };
