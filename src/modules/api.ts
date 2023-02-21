7b3f6a4c6feb5a81ee172cff840386d207571985
nariomasahisa <nariomasahisa@gmail.com>
2023-02-21 23:45:51 +0900

    feat add stockList API

--- src/modules/api.ts
+++ src/modules/api.ts
@@ -1,6 +1,21 @@
 import axios from "axios";
-import { refreshTokenState } from "../libs/recoilAtoms";
 
+//interface
+export type StockInterface = {
+    info: StockDataInfoInterface;
+};
+export type StockDataInfoInterface = {
+    Code: string;
+    CompanyName: string;
+    Date: string;
+    MarketCode: string;
+    MarketCodeName: string;
+    ScaleCategory: string;
+    Sector17Code: string;
+    Sector17CodeName: string;
+    Sector33Code: string;
+    Sector33CodeName: string;
+};
 //baseUrl
 const baseUrl = "https://api.jpx-jquants.com";
 const version = "v1";
@@ -29,5 +44,21 @@ export const getIdTokenAPI = async (refreshToken: string) => {
     const response = await axios.post(
         `${baseUrl}/${version}/token/auth_refresh?refreshtoken=${REFRESH_TOKEN}`
     );
+    return response.data;
+};
+
+/**
+ * getStockList
+ * @params {string} idToken
+ * @returns {Promise<StockInterface>}
+ */
+export const getStockListAPI = async (
+    idToken: string
+): Promise<StockInterface> => {
+    const response = await axios.get(`${baseUrl}/${version}/listed/info`, {
+        headers: {
+            Authorization: `Bearer ${idToken}`,
+        },
+    });
     return response.data;
 };
